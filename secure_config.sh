b78007a5c3b3fae29bfb4abb901f298ff1a3ae0c
Aleš Kučera <kuceral4@fel.cvut.cz>
2021-12-05 22:58:46 +0100

    Repair setup and opennsa roles

--- secure_config.sh
+++ secure_config.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-password_file=./opennsa/vars/passwords.yml
-private_key=./opennsa/ssl/private_key.pem
+hosts_dir=./hosts/production/
+secrets_file=secrets.yml
+private_key=files/private_key.pem
 
-echo "$password_file"
-echo "This script will configure and secure sensitive data using mkpasspwd and Ansible."
+echo "This script will configure and secure sensitive data using mkpasspwd and Ansible vault."
 echo "If you do not have mkpasswd installed on your computer, please install it by"
 echo
 echo "     apt install whois"
 echo
-read -p "WARNING: This action will overwrite $password_file, are you sure you want to continue? [Y/n]" permission
+read -r -p "WARNING: This action will overwrite $hosts_dir$secrets_file, are you sure you want to continue? [Y/n]" permission
 
-if [[ "$permission" == y ]] || [[ "$permission" == Y ]]
-then
+case $permission in 
+    [yY][eE][sS]|[yY])
 
     echo "Enter ansible user sudo password: " 
     read -s ansible_become_pass
@@ -25,23 +25,27 @@ then
     read -s postgres_user_pass
     postgres_user_pass_hashed="$(echo "$postgres_user_pass" | mkpasswd -m sha-512 /dev/stdin)"
 
-    echo "Saving changes to $password_file..."
-    echo "---" > $password_file
-    echo "ansible_become_pass: '$ansible_become_pass'" >> $password_file
-    echo "user_password: '$user_password'" >> $password_file
-    echo "user_password_hashed: '$user_password_hashed'" >> $password_file
-    echo "postgres_user_pass: '$postgres_user_pass'" >> $password_file
-    echo "postgres_user_pass_hashed: '$postgres_user_pass_hashed'" >> $password_file
-fi
+    echo "Saving changes to $hosts_dir$secrets_file..."
+    echo "---" > $hosts_dir$secrets_file
+    echo "ansible_become_pass: '$ansible_become_pass'" >> $hosts_dir$secrets_file
+    echo "user_password: '$user_password'" >> $hosts_dir$secrets_file
+    echo "user_password_hashed: '$user_password_hashed'" >> $hosts_dir$secrets_file
+    echo "postgres_user_pass: '$postgres_user_pass'" >> $hosts_dir$secrets_file
+    echo "postgres_user_pass_hashed: '$postgres_user_pass_hashed'" >> $hosts_dir$secrets_file
+    ;;
+*)
+esac
 
 read -p "Do you want to encrypt passwords and private key? [Y/n]" permission
-if [[ "$permission" == y ]] || [[ "$permission" == Y ]]
-then
-echo "Please enter password for Ansible vault"
-ansible-vault encrypt $password_file $private_key
-fi
+case $permission in 
+    [yY][eE][sS]|[yY])
+    echo "Please enter password for Ansible vault"
+    ansible-vault encrypt $hosts_dir$secrets_file $hosts_dir$private_key
+    ;;
+*)
+esac
 
 echo "Your sensitive data were encrypted. You can always decrypt your data with command"
 echo
-echo "  ansible-vault decrypt $password_file $private_key"
+echo "  ansible-vault decrypt $hosts_dir$secrets_file $hosts_dir$private_key"
 echo
